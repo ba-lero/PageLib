fa20ac719b5aef297c8740e2c3624a3bf1adc0f3
Basile LEZE--ROEBRT <tmzot5@gmail.com>
2021-11-25 11:14:36 +0100

    Ajout remonteur

--- scripts/menus.js
+++ scripts/menus.js
@@ -1,9 +1,5 @@
 const header = document.querySelector('.header');
 
-document.onload = headerShrink();
-
-document.addEventListener('scroll', headerShrink);
-
 function headerShrink () {
     if (window.scrollY == 0) {
         header.classList.remove('header-shrink');
@@ -11,6 +7,21 @@ function headerShrink () {
         header.classList.add('header-shrink');
     }
 }
+
+function displayLinkBack () {
+    const linkBack = document.querySelector(".link-back")
+    if (window.scrollY > (window.innerHeight/2)) {
+        linkBack.classList.add("display-lb")
+    } else {
+        if (linkBack.classList.contains) {
+            linkBack.classList.remove("display-lb")
+        }
+    }
+}
+
+document.onload = () => {headerShrink(); displayLinkBack()};
+document.addEventListener('scroll', displayLinkBack);
+document.addEventListener('scroll', headerShrink);
 
 const burger = document.querySelector('.burger i');
 const burgerNav = document.querySelector('.burger-nav-background');
